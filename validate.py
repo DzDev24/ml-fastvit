02238afe59200c4338f3e8a2499c2ca5da383c89
adelfr2015 <adelfr2015@gmail.com>
2025-05-01 00:13:25 +0100

    test

--- validate.py
+++ validate.py
@@ -478,6 +478,7 @@ def validate(args):
 def main():
     setup_default_logging()
     args = parser.parse_args()
+    print("+++++++++++++++++++++++++++++++++++++++++++++++++++";)
     print(args)
     model_cfgs = []
     model_names = []
